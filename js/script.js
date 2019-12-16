c9fefaec2d93b388b5491d57b00e50cfe710afb2
Marco <marco.carone@libero.it>
2019-12-16 17:30:10 +0100

    fix spazio js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
 alert("Benvenuti su Spotify");
-document.getElementById("album-1") .innerHTML = "Cambio Sottotitolo con JS";
+document.getElementById("album-1").innerHTML = "Cambio Sottotitolo con JS";
